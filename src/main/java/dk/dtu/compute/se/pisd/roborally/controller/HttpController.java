ecaadb017f05acf8a46e8c44b555bb58d3c42722
S235119 <S235119@dtu.dk>
2024-06-17 11:33:22 +0200

    Start på http controlleren

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/HttpController.java
@@ -0,0 +1,85 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import com.google.gson.Gson;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+public class HttpController {
+
+    private static final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+
+    public static String getPlayers() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://localhost:8089/players"))
+                .header("Accept", "application/json")
+                .build();
+        CompletableFuture<HttpResponse<String>> response =
+                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
+        return result;
+    }
+
+    public static String getPlayerByID(String playerID) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://localhost:8089/players/" + playerID))
+                .header("Accept", "application/json")
+                .build();
+        CompletableFuture<HttpResponse<String>> response =
+                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
+        return result;
+    }
+
+    public boolean addGame(String board, int gameID) throws Exception {
+        //String requestBody = "{\"gameID\":" + gameID + "\"board\":" + board + "}";
+        Gson gson = new Gson();
+        String requestBody = gson.toJson(board + gameID);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .uri(URI.create("http://localhost:8089/gameInfos"))
+                .header("Accept", "application/json")
+                .build();
+        try {
+            CompletableFuture<HttpResponse<String>> response =
+                    httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+            String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
+            return true;
+        } catch (Exception e1) {
+            return false;
+        }
+    }
+
+    public boolean addPlayer(Player p) throws Exception {
+        Gson gson = new Gson();
+        String requestBody = gson.toJson(p);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .uri(URI.create("http://localhost:8089/players"))
+                .header("Accept", "application/json")
+                .build();
+        try {
+            CompletableFuture<HttpResponse<String>> response =
+                    httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+            String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
+            return true;
+        } catch (Exception e1) {
+            return false;
+        }
+    }
+
+
+}
