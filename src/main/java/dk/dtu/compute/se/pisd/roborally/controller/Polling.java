20c899556a1d3bbd5c60fc0495f864a17cbb20d7
S235119 <S235119@dtu.dk>
2024-06-19 11:19:31 +0200

    Små opdateringer

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Polling.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Polling.java
@@ -1,12 +1,29 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
 
 public class Polling {
+    private static final String REST_URL = "http://localhost:8089/";
 
-    public boolean gameStarted() {
+    private final HttpClient httpClient;
+
+    private HttpController httpController = new HttpController();
+
+    public Polling() {
+        this.httpClient = HttpClient.newHttpClient();
+    }
+
+    public boolean gameStarted(int gameID) {
         //Skal tjekke om spillet er rykket til PROGRAMMING phase hos en spiller, og opdatere det hos alle
+
+           // if (httpController.getByGameID(gameID).)
+
         return true;
     }
 
@@ -16,7 +33,7 @@ public class Polling {
     }
 
     public boolean programmingCompleted() {
-        //Tjekke om alle spillere har alle 5 programming cards i databasen, og sende spillet til EXECUTION phase
+        //Tjekke om alle spillere med samme gameID har programmingDone = true
         return true;
     }
 
